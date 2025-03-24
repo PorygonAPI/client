81afa5657f591b3876bcad411edbb741a95a580f
João Arruda <joaoarruda.info@gmail.com>
2025-03-23 22:43:02 -0300

    feat(#3): adicionando primevue ao projeto

--- src/main.js
+++ src/main.js
@@ -4,7 +4,26 @@ import router from './router'
 import axiosPlugin from './plugins/axios'
 import './assets/main.css'
 
+import PrimeVue from 'primevue/config';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Aura from '@primeuix/themes/aura';
+
 const app = createApp(App)
 app.use(router)
 app.use(axiosPlugin)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: "primevue",
+        order: "base, primevue",
+      },
+      darkModeSelector: ".dark",
+    },
+  },
+});app.component('DataTable', DataTable);
+app.component('DataTableColumn', Column);
 app.mount('#app')
