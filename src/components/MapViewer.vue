9d446401e91f898864c56254446bab0d50cb0372
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-04-21 13:57:31 -0300

    feat (#25): Testes com mais de um poligono
    - Mostrando mais de um polígono, se o geoJson de parâmetro tiver mais de um
    - Mudando a cor do polígono se ele for da classe DANINHA
    -Função para centralizar o mapa automaticamente para a primeira coordenada do primeiro polígono
    
    Melhorias:
    -Validar formato do json a ser inserido/lido pela API (ie se o campo 'proprieties' é filha ou parente de 'geometry'

--- src/components/MapViewer.vue
+++ src/components/MapViewer.vue
@@ -6,6 +6,7 @@
 
 <template>
     <h2>Map Viewer Test</h2>
+    <p>{{ talhoesGeoJson }}</p>
     <div id="map"></div>
 </template>
 
@@ -16,7 +17,7 @@ import L from 'leaflet'
 
 export default defineComponent({
     props: {
-        geoJson: {
+        talhoesGeoJson: {
             type: Array,
             required: false,
             default: () => []
@@ -24,9 +25,10 @@ export default defineComponent({
     },
     setup(props) {
         let map = null
+        let talhoesParsed = null
+        let mapLocation = []
 
         onMounted(() => {
-            console.log(props.geoJson)
             createMapLayer()
         })
 
@@ -36,21 +38,28 @@ export default defineComponent({
             }
         })
 
-        
+
         const createMapLayer = () => {
+
+            talhoesParsed = JSON.parse(props.talhoesGeoJson)
+
+            //Define a variável de posição do mapa que usada na linha abaixo (50)
+            setLocalizacaoMapa()
+
             //Mostrar o mapa na tela
-            map = L.map('map').setView([48.99, -104.05], 4)
+            map = L.map('map').setView(mapLocation, 6)
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution:
                     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map)
 
             //Inserindo o geoJson no mapa
-            setMarkers()
+            setTalhoes()
         }
 
-        const setMarkers = () => {
+        const setTalhoes = () => {
 
+            //***TESTE PONTO***
             // var someFeatures = {
             //     "type": "Feature",
             //     "properties": {
@@ -61,44 +70,86 @@ export default defineComponent({
             //     }
             // };
 
-            var someFeatures = [{
-                "type": "Feature",
-                "properties": { "party": "Republican" },
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[
-                        [-104.05, 48.99],
-                        [-97.22, 48.98],
-                        [-96.58, 45.94],
-                        [-104.03, 45.94],
-                        [-104.05, 48.99]
-                    ]]
-                }
-            },
-            {
-                "type": "Feature",
-                "properties": { "party": "Democrat" },
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [[
-                        [-109.05, 41.00],
-                        [-102.06, 40.99],
-                        [-102.03, 36.99],
-                        [-109.04, 36.99],
-                        [-109.05, 41.00]
-                    ]]
-                }
-            }
-            ];
+            //  ***TESTE GEOJSONPOLIGONO E ESTILIZACAO***
+            // var someFeatures = [{
+            //     "type": "Feature",
+            //     "properties": { "party": "Republican" },
+            //     "geometry": {
+            //         "type": "Polygon",
+            //         "coordinates": [[
+            //             [-104.05, 48.99],
+            //             [-97.22, 48.98],
+            //             [-96.58, 45.94],
+            //             [-104.03, 45.94],
+            //             [-104.05, 48.99]
+            //         ]]
+            //     }
+            // },
+            // {
+            //     "type": "Feature",
+            //     "properties": { "party": "Democrat" },
+            //     "geometry": {
+            //         "type": "Polygon",
+            //         "coordinates": [[
+            //             [-109.05, 41.00],
+            //             [-102.06, 40.99],
+            //             [-102.03, 36.99],
+            //             [-109.04, 36.99],
+            //             [-109.05, 41.00]
+            //         ]]
+            //     }
+            // }
+            // ];
 
-            L.geoJSON(someFeatures, {
+            // L.geoJSON(someFeatures, {
+            //     style: function (feature) {
+            //         switch (feature.properties.party) {
+            //             case 'Republican': return { color: "#ff0000" };
+            //             case 'Democrat': return { color: "#0000ff" };
+            //         }
+            //     }
+            // }).addTo(map);
+
+            // console.log(JSON.stringify(someFeatures))
+
+            L.geoJSON(talhoesParsed, {
                 style: function (feature) {
-                    switch (feature.properties.party) {
-                        case 'Republican': return { color: "#ff0000" };
-                        case 'Democrat': return { color: "#0000ff" };
+                    console.log(JSON.stringify(feature))
+                    switch (feature.geometry.crs.properties.CLASSE) {
+                        //DANINHAS -> Vermelho, Resto -> Azul
+                        case 'DANINHAS': return { color: "#ff0000" };
+                        default: return { color: "#0000ff" };
                     }
                 }
             }).addTo(map);
+        }
+
+        const setLocalizacaoMapa = () => {
+
+            if (talhoesParsed.length > 0) {
+
+                if (talhoesParsed[0].coordinates.length > 0) {
+
+                    if (talhoesParsed[0].coordinates[0].length > 0) {
+
+                        //talhoesParsed[0].coordinates[0] retona um vetor com todas as coordenadas (ie [[20,21], [22,23]])
+                        //Logo, talhoesParsed[0].coordinates[0][0] retorna a primeira coordenada (ie [20,21])
+                        //Portanto talhoesParsed[0].coordinates[0][0][0] retorna o primeiro valor da coordenada (ie 20)
+                        //Já talhoesParsed[0].coordinates[0][0][0] retorna o segundo (ie 21)
+
+                        //Por algum motivo o setView recebe os parametros invertidos, se um geoJson possui uma coordenada [20,21] por exemplo,
+                        //a localização na setView da linha 50 deve ser inserida como [21,20] para que o mapa fica centralizado no mesmo ponto que o geoJSON
+                        //Se não inverter a posição, o mapa fica centralizado em uma área nada a ver bem distante do ponto
+                        let first = talhoesParsed[0].coordinates[0][0][1]
+                        let last = talhoesParsed[0].coordinates[0][0][0]
+                        mapLocation.push(first)
+                        mapLocation.push(last)
+                    }
+
+                }
+
+            }
+
         }
     }
 })
--- src/views/TesteTalhaoView.vue
+++ src/views/TesteTalhaoView.vue
@@ -6,13 +6,19 @@
                 <p class="text-4xl font-semibold text-gray-800">Teste Visualizador de Talhão</p>
             </div>
             <hr class="border-gray-300 mb-4">
-            <MapViewer />
+            <MapViewer :talhoes-geo-json="fullGeoJson"/>
         </div>
     </div>
 </template>
-<!-- 
-<script setup lang="ts"> -->
 
 <script setup>
 import MapViewer from '@/components/MapViewer.vue';
+
+const talhoesGeoJson = '[{"type":"Polygon","coordinates":[[[-46.62529,-23.533773],[-46.62529,-23.534773],[-46.62429,-23.534773],[-46.62429,-23.533773],[-46.62529,-23.533773]]],"crs":{"type":"name","properties":{"name":"EPSG:4326"}}},'
+
+const daninhasGeoJson = '{"type":"Polygon","coordinates":[[[-46.3446028104612,-23.64587273889485],[-46.3446028104612,-20.64587519603432],[-46.344605505407046,-20.64587519603432],[-49.344605505407046,-20.645880585926026],[-49.34461089529875,-20.645880585926026],[-49.34461089529875,-20.645873311398894],[-49.3446028104612,-20.64587273889485]]],"crs":{"type":"name","properties":{"name":"EPSG:4326", "CLASSE": "DANINHAS"}}}]'
+
+const fullGeoJson = talhoesGeoJson + daninhasGeoJson;
+
+
 </script>
