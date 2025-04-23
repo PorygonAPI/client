96435275f56f4ddb0356ee4afed439116f6618a4
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2025-04-23 00:10:25 -0300

    feat(#23): Alterações iniciais em AreaAgricolasView

--- src/views/AreasAgricolasView.vue
+++ src/views/AreasAgricolasView.vue
@@ -1,11 +1,27 @@
 <template>
+  <div class="h-full w-[90%] ml-[5%] mr-[5%]">
+    <div class="flex flex-col">
 
-  <p class="text-center my-5 font-semibold text-2xl"> Teste</p>
+      <div class="text-center p-2 mt-4 lg:mb-3 mb-1">
+        <p class="text-4xl font-semibold text-gray-800">Áreas Agrícolas</p>
+      </div>
 
- <div class="flex w-full justify-center gap-5 mt-1">
-  <RouterLink to="/areasagro/cadastrofazenda" class="bg-gray-400 hover:bg-gray-500 p-1.5 text-white"> Cadastro de Fazenda</RouterLink>
-  <RouterLink to="/areasagro/cadastrotalhao" class="bg-gray-400 hover:bg-gray-500 p-1.5 text-white"> Cadastro de Talhão</RouterLink>
- </div>
+      <hr class="border-gray-300 mb-4">
 
+      <div class="bg-white shadow rounded-xl p-5">
+
+        <div class="flex gap-3">
+          <span class=" p-1 px-2 rounded-lg shadow text-white border-gray-300 bg-gray-400 hover:bg-gray-300 transition flex items-center justify-center "><button>Fazendas</button></span>
+          <span class=" p-1 px-2 rounded-lg shadow text-white border-gray-300 bg-gray-400 hover:bg-gray-300 transition flex items-center justify-center "><button>Talhões</button></span>
+        </div>
+
+        <div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </div>
 </template>
 
